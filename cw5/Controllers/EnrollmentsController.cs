435f8ef5accad8241db07c882eac4dfcabfa035e
unknown <s18911@pjwstk.edu.pl>
2020-04-19 22:15:08 +0200

    cw6

---
+++ cw5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using cw5.Models;
+using cw5.Request;
+using Microsoft.AspNetCore.Mvc;
+using cw5.Services;
+
+namespace cw5.Controllers{
+
+
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase{
+        private Student student;
+        private IStudentDbService dbService;
+
+        public EnrollmentsController(IStudentDbService dbService) {
+            this.dbService = dbService;
+        }
+
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest studentRequest) {
+            MapStudent(studentRequest);
+
+            if (dbService.CheckStudiesDb(studentRequest)){
+                return Ok(dbService.GetEnrollment());//Created(Url,dbService.GetEnrollment());
+            }
+            return NotFound();
+        }
+
+        [Route("/api/enrollments/promotions")]
+        [HttpPost]
+        public IActionResult PromoteStudents(Promotions promotions) {
+            if (dbService.PromoteStudents(promotions)){
+                return Ok(dbService.GetEnrollment());//Created(Url,dbService.GetEnrollment());
+            }
+            else {
+                return NotFound();
+            }
+        }
+
+        public void MapStudent(EnrollStudentRequest enrollRequest) {
+            student = new Student();
+
+            student.IndexNumber = enrollRequest.IndexNumber;
+            student.FirstName = enrollRequest.FirstName;
+            student.LastName = enrollRequest.LastName;
+            student.Birthdate = enrollRequest.Birthdate;
+
+        }
+
+        
+    }
+}
---
+++ cw5/Services/IStudentDbService.cs
@@ -0,0 +1,21 @@
+﻿using cw3.Models;
+using cw5.Models;
+using cw5.Request;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw5.Services
+{
+    public interface IStudentDbService{
+        bool CheckStudiesDb(EnrollStudentRequest enrollStudent);
+
+        bool PromoteStudents(Promotions promotions);
+
+        public Enrollment GetEnrollment();
+
+        public Student GetStudent(string index);
+    }
+}
